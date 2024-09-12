3913849c9dd9af4071066f5d879f5fded4745ddf
orwrandal <randypichardo1987@gmail.com>
2024-09-12 13:08:43 -0400

    Added saved route and removed body spacing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import AnimalBreed from "./pages/AnimalBreeds";
 import Quiz from "./pages/Quiz";
 import AdoptInfo from "./pages/AdoptInfo";
 import BreedInfo from "./pages/BreedInfo";
+import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
       <Route path="/animalbreed" element={<AnimalBreed />} />
       <Route path="/AdoptInfo/:id" element={<AdoptInfo />} />
       <Route path="/BreedInfo/:id" element={<BreedInfo />} />
+      <Route path="/saved" element={<Saved />} />
     </Routes>
   );
 }
