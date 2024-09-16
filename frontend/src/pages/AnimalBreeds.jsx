0cc6967bec18c116d30121492f91068e922645bd
orwrandal <randypichardo1987@gmail.com>
2024-09-16 12:59:42 -0400

    Functionaility wise its done

--- frontend/src/pages/AnimalBreeds.jsx
+++ frontend/src/pages/AnimalBreeds.jsx
@@ -1,23 +1,92 @@
+import { useContext } from "react";
+import BreedCard from "../components/breedCard";
+import GlobalContext from "../context/globalContext";
 import { useState } from "react";
+import { useEffect } from "react";
 
 function AnimalBreed() {
-  const [activeTab, setActiveTab] = useState('cats');
+  const {activeTab, setActiveTab, breeds, setBreedQuery, breedLoading,setCardRating, cardRating, catAttributes} = useContext(GlobalContext);
+  const [input, setInput] = useState('');
+  const [catsDropdown, setCatsDropdown] = useState(false);
+
+  useEffect(() => {
+    setCatsDropdown(false)
+  }, [cardRating])
 
   return (
     <>
     <h1 className="is-size-2 has-text-weight-bold has-text-centered mt-6 mb-4 has-text-black">Search For Breeds</h1>
-      <div className="field" id='breedForm'>
+      <form id='breedForm' onSubmit={(e) => {
+        e.preventDefault()
+        setBreedQuery(input)
+      }}>
+      <div className="field has-addons" >
         <p className="control has-icons-left has-icons-right">
-        <input className="input has-background-white" type="email" placeholder="Search For Breed" />
+        <input 
+        className="input has-background-white"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Search For Breed" />
         <span className="icon is-small is-left" >
           <svg id='breedSearchSvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#D3D3D3" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6 .1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg>
         </span>
         </p>
+        <div className="control">
+          <button className="button">
+            Search
+          </button>
+          
       </div>
+      
+      </div>
+      {activeTab === 'cats'? 
+      <div className= {`dropdown ${catsDropdown? "is-active": ""}`}>
+        <div className="dropdown-trigger"  onClick={() => setCatsDropdown(!catsDropdown)}>
+          <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" id="catDropdown">
+            <span>{catAttributes[cardRating] || 'Cat Rating Property'}</span>
+            <span className="icon is-small">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#000000" d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
+            </span>
+          </button>
+        </div>
+        <div className="dropdown-menu attributeDrop" id="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            <a className="dropdown-item" onClick={() => setCardRating('affection_level')}>Affection Level</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('adaptability')}>Adaptability</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('child_friendly')}>Child Friendly</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('dog_friendly')}>Dog Friendly</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('energy_level')}>Energy Level</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('grooming')}>Grooming</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('health_issues')}>Health Issues</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('intelligence')}>Intelligence</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('shedding_level')}>Shedding Level</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('social_needs')}>Social Needs</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('stranger_friendly')}>Stranger Friendly</a>
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item" onClick={() => setCardRating('vocalisation')}>Vocalisation</a>
+          </div>
+        </div>
+      </div>:
+      <>
+      </>}
+      </form>
+
+      
 
       <div className="tabs is-centered">
         <ul>
-          <li className={activeTab === 'cats'? 'is-active': ''} onClick={() => setActiveTab('cats')}>
+          <li className={activeTab === 'cats'? 'is-active': 'hoverTab'} onClick={() => setActiveTab('cats')}>
             <a>
               <span className="icon is-small">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill={activeTab === 'cats'? '#8895ff': '#afb0bb'} d="M320 192l17.1 0c22.1 38.3 63.5 64 110.9 64c11 0 21.8-1.4 32-4l0 4 0 32 0 192c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-140.8L280 448l56 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-144 0c-53 0-96-43-96-96l0-223.5c0-16.1-12-29.8-28-31.8l-7.9-1c-17.5-2.2-30-18.2-27.8-35.7s18.2-30 35.7-27.8l7.9 1c48 6 84.1 46.8 84.1 95.3l0 85.3c34.4-51.7 93.2-85.8 160-85.8zm160 26.5s0 0 0 0c-10 3.5-20.8 5.5-32 5.5c-28.4 0-54-12.4-71.6-32c0 0 0 0 0 0c-3.7-4.1-7-8.5-9.9-13.2C357.3 164 352 146.6 352 128c0 0 0 0 0 0l0-96 0-20 0-1.3C352 4.8 356.7 .1 362.6 0l.2 0c3.3 0 6.4 1.6 8.4 4.2c0 0 0 0 0 .1L384 21.3l27.2 36.3L416 64l64 0 4.8-6.4L512 21.3 524.8 4.3c0 0 0 0 0-.1c2-2.6 5.1-4.2 8.4-4.2l.2 0C539.3 .1 544 4.8 544 10.7l0 1.3 0 20 0 96c0 17.3-4.6 33.6-12.6 47.6c-11.3 19.8-29.6 35.2-51.4 42.9zM432 128a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm48 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32z"/></svg>
@@ -25,7 +94,7 @@ function AnimalBreed() {
               <span>Cats</span>
             </a>
           </li>
-          <li className={activeTab === 'dogs'? 'is-active': ''} onClick={() => setActiveTab('dogs')}>
+          <li className={activeTab === 'dogs'? 'is-active': 'hoverTab'} onClick={() => setActiveTab('dogs')}>
             <a>
               <span className="icon is-small">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill={activeTab === 'dogs'? '#8895ff': '#afb0bb'} d="M309.6 158.5L332.7 19.8C334.6 8.4 344.5 0 356.1 0c7.5 0 14.5 3.5 19 9.5L392 32l52.1 0c12.7 0 24.9 5.1 33.9 14.1L496 64l56 0c13.3 0 24 10.7 24 24l0 24c0 44.2-35.8 80-80 80l-32 0-16 0-21.3 0-5.1 30.5-112-64zM416 256.1L416 480c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-115.2c-24 12.3-51.2 19.2-80 19.2s-56-6.9-80-19.2L160 480c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-230.2c-28.8-10.9-51.4-35.3-59.2-66.5L1 167.8c-4.3-17.1 6.1-34.5 23.3-38.8s34.5 6.1 38.8 23.3l3.9 15.5C70.5 182 83.3 192 98 192l30 0 16 0 159.8 0L416 256.1zM464 80a16 16 0 1 0 -32 0 16 16 0 1 0 32 0z"/></svg>
@@ -34,6 +103,15 @@ function AnimalBreed() {
             </a>
           </li>
         </ul>
+      </div>
+      <div id='card-holder'>
+        {breedLoading? <>
+          <p>Loading</p>
+        </>: breeds?.map((breed) => {
+          return (
+            <BreedCard breed={breed} />
+          );
+        })}
       </div>
     </>
   );
